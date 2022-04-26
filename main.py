3cf9765411f81c71cc9a87e8691fe737c314f632
Abdulaziz Al Owain <aziz.alowain7@gmail.com>
2022-04-26 11:39:12 +0300

    Final commit using discord.py
    This is the final commit before moving to discord.js and this is just an archival change

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 import os
 import discord
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+# -----------------------------
+# -- B o i l e r   P l a t e --
+# -----------------------------
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+config = {
+    'prefix': '>', # Bot prefix
+    'version': '0.0.0', # Bot version
+    'guild': 733369204748583095, # Server ID
 
-API_KEY = os.getenv('DISCORD_BOT')
+    # FOR TEST USE ONLY, PLEASE ADD AN API_KEY TO YOUR ENVIROMENT VARIABLES
+    'API_KEY': False
+}
+
+API_KEY = config['API_KEY'] if config['API_KEY'] else os.getenv('DISCORD_BOT')
 if not API_KEY:
-    print('You have not set an API KEY.')
-else:
-    client = MyClient()
-    client.run(API_KEY)
+    exit('You have not set an API KEY.\nAdd a Discord API key to your enviroment variables.\nAlternatively, you can add it to your config for testing purposes.')
+
+
+# -----------------------
+# -- B o t   C l a s s --
+# -----------------------
+
+
+class Bot(discord.Client):
+    def __init__(self):
+        self.prefix = config['prefix']
+        self.ready = False
+
+    async def on_connect(self):
+        print('Connected')
+    async def on_disconnect(self):
+        print('Disconnected')
+
+    # async def on_ready(self):
+    #     if not self.ready:
+    #         self.ready = True
+    #         self.guild = self.get_guild(config['guild'])
+    #     else:
+    #         print('Reconnected')
+
+
+bot = Bot()
+
+bot.run(API_KEY)
